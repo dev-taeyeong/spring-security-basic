81e98a63984d4c560adc73c328c2fed2f3edd6ae
taeyeong123 <taeyeong@cherground.com>
2023-05-02 11:57:47 +0900

    remember me 인증
    - RememberMeAuthenticationFilter 학습

--- src/main/java/io/security/securitybasic/SecurityConfig.java
+++ src/main/java/io/security/securitybasic/SecurityConfig.java
@@ -1,11 +1,13 @@
 package io.security.securitybasic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -21,53 +23,68 @@ import java.io.IOException;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated();
-        http
-                .formLogin()
-                // .loginPage("/loginPage")
-                .defaultSuccessUrl("/")
-                .failureUrl("/login")
-                .usernameParameter("userId")
-                .passwordParameter("passwd")
-                .loginProcessingUrl("/login_proc")
-                // .successHandler(new AuthenticationSuccessHandler() {
-                //     @Override
-                //     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                //         System.out.println("authentication: " + authentication.getName());
-                //         response.sendRedirect("/");
-                //     }
-                // })
-                // .failureHandler(new AuthenticationFailureHandler() {
-                //     @Override
-                //     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                //         System.out.println("exception: " + exception.getMessage());
-                //         response.sendRedirect("/login");
-                //     }
-                // })
-                .permitAll()
-        ;
-        http
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .addLogoutHandler(new LogoutHandler() {
-                    @Override
-                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-                        HttpSession session = request.getSession();
-                        session.invalidate();
-                    }
-                })
-                .logoutSuccessHandler(new LogoutSuccessHandler() {
-                    @Override
-                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        response.sendRedirect("/login");
-                    }
-                })
-                .deleteCookies("remember-me")
-        ;
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
+
+        http.formLogin();
+
+        http.rememberMe()
+                .userDetailsService(userDetailsService);
+
+//        http.formLogin()
+//                 .loginPage("/loginPage")
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/login")
+//                .usernameParameter("userId")
+//                .passwordParameter("passwd")
+//                .loginProcessingUrl("/login_proc")
+//                 .successHandler(new AuthenticationSuccessHandler() {
+//                     @Override
+//                     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//                         System.out.println("authentication: " + authentication.getName());
+//                         response.sendRedirect("/");
+//                     }
+//                 })
+//                 .failureHandler(new AuthenticationFailureHandler() {
+//                     @Override
+//                     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+//                         System.out.println("exception: " + exception.getMessage());
+//                         response.sendRedirect("/login");
+//                     }
+//                 })
+//                .permitAll()
+//        ;
+
+//        http.logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/login")
+//                .addLogoutHandler(new LogoutHandler() {
+//                    @Override
+//                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+//                        HttpSession session = request.getSession();
+//                        session.invalidate();
+//                    }
+//                })
+//                .logoutSuccessHandler(new LogoutSuccessHandler() {
+//                    @Override
+//                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//                        response.sendRedirect("/login");
+//                    }
+//                })
+//                .deleteCookies("remember-me")
+//        ;
+
+//        http.rememberMe()
+//                .rememberMeParameter("remember")
+//                .tokenValiditySeconds(3600)
+//                .alwaysRemember(false)
+//                .userDetailsService(userDetailsService)
+//        ;
     }
 }
